d548beed24af6684a3b4ea03797dc60cc684bc92
Lamis213 <lamismater213@gmail.com>
2025-05-13 23:48:55 +0300

    Add Trello Power-Up button functionality

---
+++ client.js
@@ -0,0 +1,16 @@
+TrelloPowerUp.initialize({
+  'card-buttons': function(t, options) {
+    return [{
+      icon: 'https://cdn-icons-png.flaticon.com/512/906/906175.png',
+      text: 'Open Katalon Report',
+      callback: function(t) {
+        return t.popup({
+          title: 'Katalon Report',
+          url: './index.html',
+        });
+      }
+    }];
+  }
+});
+
+
